fd9e2882708eac08cd5c91a4f23b8feb2e0e2f80
Robert Pratte <robert.pratte@fedex.com>
2021-07-27 09:28:43 -0400

    splunk

--- Libraries/splunk/steps/splunk_step_watcher.groovy
+++ Libraries/splunk/steps/splunk_step_watcher.groovy
@@ -3,6 +3,4 @@ void before(){
     println "Splunk: running before the ${hookContext.library} library's ${hookContext.step} step"
 }
 
-@AfterStep
-    ({ hookContext.step.equals("static_code_analysis") })
-    
+@AfterStep({ hookContext.step.equals("static_code_analysis") })
